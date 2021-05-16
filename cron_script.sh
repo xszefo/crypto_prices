ccc9cce6c114728aa68d1b80410a5d6de1dc8151
xszefo <piotr.sumelka@gmail.com>
2021-05-16 15:00:42 +0200

    change in cron script

--- cron_script.sh
+++ cron_script.sh
@@ -2,5 +2,5 @@ uptime
 cd '/home/piotr/Nauka/crypto_env'
 source 'bin/activate'
 cd '/home/piotr/Nauka/crypto_env/crypto_prices'
-/home/piotr/Nauka/crypto_env/bin/python 'get_prices.py'
+/usr/bin/python3 'get_prices.py'
 echo '**************************'
