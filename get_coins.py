3eac94cd1a1ea39d6bd23540d154107ea6817c04
Piotr Sumelka <piotr.sumelka@gmail.com>
2023-03-04 21:58:54 +0100

    changes by black

--- get_coins.py
+++ get_coins.py
@@ -2,31 +2,32 @@ import requests
 import json
 from headers import headers_crypto
 
+
 def get_coins_details():
-        url = "https://coinranking1.p.rapidapi.com/coins"
-        response = requests.request("GET", url, headers=headers_crypto)
+    url = "https://coinranking1.p.rapidapi.com/coins"
+    response = requests.request("GET", url, headers=headers_crypto)
 
-        resp_json = response.json()
+    resp_json = response.json()
 
-        my_coins = ['eth', 'btc', 'doge', 'shib']#, 'xrp', 'ltc', 'bch', 'eos']
+    my_coins = ["eth", "btc", "doge", "shib"]  # , 'xrp', 'ltc', 'bch', 'eos']
+    list_of_my_coins = []
+    for coin in resp_json["data"]["coins"]:
+        if coin["symbol"].lower() in my_coins:
+            temp_dict = {
+                "coin_id": coin["id"],
+                "coin_name": coin["name"],
+                "coin_symbol": coin["symbol"],
+            }
+            list_of_my_coins.append(temp_dict)
 
-        list_of_my_coins = []
+    return list_of_my_coins
 
-        for coin in resp_json['data']['coins']:
-                if coin['symbol'].lower() in my_coins:
-                        temp_dict = {
-                                'coin_id': coin['id'],
-                                'coin_name': coin['name'],
-                                'coin_symbol': coin['symbol']
-                        }
-                        list_of_my_coins.append(temp_dict)
-
-        return list_of_my_coins
 
 def main():
-        my_coins = get_coins_details()
-        with open('my_coins', 'w') as f:
-                json.dump(my_coins, f)
+    my_coins = get_coins_details()
+    with open("my_coins", "w") as f:
+        json.dump(my_coins, f)
 
-if __name__ == '__main__':
-        main()
+
+if __name__ == "__main__":
+    main()
